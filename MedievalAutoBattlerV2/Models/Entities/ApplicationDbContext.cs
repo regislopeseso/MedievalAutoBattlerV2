023f075068c17808354ee6e8635b310ae548e18e
Regis <regis.lopes.eso@gmail.com>
2025-01-31 10:58:56 -0300

    DbContext added and configured

---
+++ MedievalAutoBattlerV2/Models/Entities/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MedievalAutoBattlerV2.Models.Entities
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- MedievalAutoBattlerV2/Program.cs
+++ MedievalAutoBattlerV2/Program.cs
@@ -1,7 +1,20 @@
+using MedievalAutoBattlerV2.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseMySql(
+        connectionString,
+        ServerVersion.AutoDetect(connectionString),
+        options => { options.CommandTimeout(120); }
+        );
+});
 
 var app = builder.Build();
 
